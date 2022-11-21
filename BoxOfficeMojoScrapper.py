68a0bc4f38250435bf9538789862d9576e4b261c
Agustin Normand <normandagustin@gmail.com>
2022-11-21 16:59:08 -0300

    Add detect for duplicated movies

--- BoxOfficeMojoScrapper.py
+++ BoxOfficeMojoScrapper.py
@@ -8,9 +8,9 @@ import logging
 
 BOX_OFFICE_MOJO_BASE_URL = "https://www.boxofficemojo.com"
 BOX_OFFICE_MOJO_OPENINGS_URL = BOX_OFFICE_MOJO_BASE_URL+"/chart/top_opening_weekend/"
-SECONDS_TO_SLEEP_BETWEEN_REQUESTS = 0
+SECONDS_TO_SLEEP_BETWEEN_REQUESTS = 0.2
 SAVE_HTMLS = False
-LOGGING_LEVEL = logging.DEBUG
+LOGGING_LEVEL = logging.WARNING
 LOG_FILENAME = './app.log'
 
 #logging.basicConfig(level=LOGGING_LEVEL, filename=LOG_FILENAME, filemode='w', format='%(name)s - %(levelname)s - %(message)s')
@@ -21,24 +21,27 @@ def request_mojo(offset):
   r = requests.get(headers=Headers().generate(), url=url_with_offset)
   return [r.status_code, r.text]
 
-def parse_response_page_mojo(html_response):
+def parse_response_page_mojo(html_response, movies):
   soup = BeautifulSoup(html_response, "html.parser")
   table = soup.find("div", {"id":"table"})
   release_fields = table.find_all("td", {"class": "mojo-field-type-release"})
 
-  movies = {}
   for release_field in release_fields:
-    movies[release_field.text] = {}
-    movies[release_field.text]["link"] = BOX_OFFICE_MOJO_BASE_URL+release_field.a["href"]
-  print(len(movies.keys()))
+    movie_name = release_field.text
+    if movie_name in movies.keys():
+      logging.warning("Movie name was already in movies dictionary {}".format(movie_name))
+    else:
+      movies[movie_name] = {}
+    movies[movie_name]["link"] = BOX_OFFICE_MOJO_BASE_URL+release_field.a["href"]
 
+movies = {}
 for offset in [0, 200, 400, 600, 800]:
   status_code, text_response = request_mojo(offset)
   logging.debug("New request of page efectuated")
   logging.debug("Status Code {}".format(status_code))
   logging.debug("Requested with offset {} ".format(offset))
   logging.debug("Response len {}".format(len(text_response)))
-  parse_response_page_mojo(text_response)
+  parse_response_page_mojo(text_response, movies)
 
   if SAVE_HTMLS:
     filename = "./initial_html_responses/offset_{}.html".format(offset)
@@ -50,4 +53,4 @@ for offset in [0, 200, 400, 600, 800]:
     logging.debug("Sleeping {} second to avoid bans".format(SECONDS_TO_SLEEP_BETWEEN_REQUESTS))
     time.sleep(SECONDS_TO_SLEEP_BETWEEN_REQUESTS)
 
-  break
+print(len(movies))
