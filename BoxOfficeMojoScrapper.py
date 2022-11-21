6bdde161027a4e83e913e115d19d2ce9f5de9279
Agustin Normand <normandagustin@gmail.com>
2022-11-21 17:52:39 -0300

    Change key to incremental id

--- BoxOfficeMojoScrapper.py
+++ BoxOfficeMojoScrapper.py
@@ -16,32 +16,37 @@ LOG_FILENAME = './app.log'
 #logging.basicConfig(level=LOGGING_LEVEL, filename=LOG_FILENAME, filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 logging.basicConfig(level=LOGGING_LEVEL)
 
-def request_mojo(offset):
+def do_request(url):
+def do_request(url):
+  r = requests.get(headers=Headers().generate(), url=url)
+  return r
+
+def request_mojo_page(offset):
   url_with_offset = "{}?offset={}".format(BOX_OFFICE_MOJO_OPENINGS_URL, offset)
-  r = requests.get(headers=Headers().generate(), url=url_with_offset)
+  r = do_request(url_with_offset)
   return [r.status_code, r.text]
 
-def parse_response_page_mojo(html_response, movies):
+def parse_response_page_mojo(html_response, movies, incremental_id):
   soup = BeautifulSoup(html_response, "html.parser")
   table = soup.find("div", {"id":"table"})
   release_fields = table.find_all("td", {"class": "mojo-field-type-release"})
 
   for release_field in release_fields:
     movie_name = release_field.text
-    if movie_name in movies.keys():
-      logging.warning("Movie name was already in movies dictionary {}".format(movie_name))
-    else:
-      movies[movie_name] = {}
-    movies[movie_name]["link"] = BOX_OFFICE_MOJO_BASE_URL+release_field.a["href"]
+    movies[incremental_id] = {}
+    movies["movie_name"] = movie_name
+    movies[incremental_id]["link"] = BOX_OFFICE_MOJO_BASE_URL+release_field.a["href"]
+    incremental_id += 1
 
+incremental_id = 0
 movies = {}
 for offset in [0, 200, 400, 600, 800]:
-  status_code, text_response = request_mojo(offset)
+  status_code, text_response = request_mojo_page(offset)
   logging.debug("New request of page efectuated")
   logging.debug("Status Code {}".format(status_code))
   logging.debug("Requested with offset {} ".format(offset))
   logging.debug("Response len {}".format(len(text_response)))
-  parse_response_page_mojo(text_response, movies)
+  parse_response_page_mojo(text_response, movies, incremental_id)
 
   if SAVE_HTMLS:
     filename = "./initial_html_responses/offset_{}.html".format(offset)
@@ -52,5 +57,14 @@ for offset in [0, 200, 400, 600, 800]:
   if SECONDS_TO_SLEEP_BETWEEN_REQUESTS > 0:
     logging.debug("Sleeping {} second to avoid bans".format(SECONDS_TO_SLEEP_BETWEEN_REQUESTS))
     time.sleep(SECONDS_TO_SLEEP_BETWEEN_REQUESTS)
+  break #Delete this to query all pages #TODO
 
-print(len(movies))
+logging.debug("Scraped {} movies from initial 5-pages".format(len(movies)))
+
+for movie_id in movies:
+  r = do_request(movies[movie_id]["link"])
+  if r.status_code != 200:
+    logging.warning("Status code != 200 in {}, Movie name {}".format(movies[movie_id]["link"], movies[movie_id]["movie_name"]))
+  print(r.text)
+  break
+
