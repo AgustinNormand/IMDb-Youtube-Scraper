5ce1785782f291e645090bd67c1a183ae9ac7908
Agustin Normand <normandagustin@gmail.com>
2022-12-01 20:10:14 -0300

    I create the constant for the worst opening weekend scraper

--- constants.py
+++ constants.py
@@ -1,6 +1,7 @@
 SECONDS_TO_SLEEP_BETWEEN_REQUESTS = 0
 BOX_OFFICE_MOJO_BASE_URL = "https://www.boxofficemojo.com"
 BOX_OFFICE_MOJO_OPENINGS_URL = BOX_OFFICE_MOJO_BASE_URL + "/chart/top_opening_weekend/"
+BOX_OFFICE_MOJO_WORST_OPENINGS_URL = BOX_OFFICE_MOJO_BASE_URL + "/chart/btm_wide_opening_weekend_theater_avg/"
 IMDb_URL = "https://www.imdb.com"
 
 MONTHS = {
