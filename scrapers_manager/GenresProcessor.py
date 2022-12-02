ee90fff2c216dfe925611b370fa05e7b43a1b9b3
Agustin Normand <normandagustin@gmail.com>
2022-12-02 09:29:57 -0300

    Fix bug in genres processor, add json files to gitignore

--- scrapers_manager/GenresProcessor.py
+++ scrapers_manager/GenresProcessor.py
@@ -4,24 +4,31 @@ class GenresProcessor:
 
     def obtain_unique_genres_list(self, movies):
         for movie in movies:
-            for genre in movie["genres"]:
-                if genre not in self.unique_genres:
-                    self.unique_genres.append(genre)
+            if "genres" in movie.keys():
+                for genre in movie["genres"]:
+                    if genre not in self.unique_genres:
+                        self.unique_genres.append(genre)
 
     def create_genres_columns(self, movies):
         for index, movie in enumerate(movies):
             for genre in self.unique_genres:
-                if genre not in movie["genres"]:
-                    movies[index][genre] = 0
+                if "genres" in movie.keys():
+                    if genre not in movie["genres"]:
+                        movies[index][genre] = 0
+                    else:
+                        movies[index][genre] = 1
                 else:
-                    movies[index][genre] = 1
+                    movies[index][genre] = 0
+        return movies
 
     def drop_genres_column(self, movies):
         for movie in movies:
-            movie.pop("genres")
+            if "genres" in movie.keys():
+                movie.pop("genres")
+        return movies
 
     def process_genres(self, movies):
         self.obtain_unique_genres_list(movies)
-        #movies = self.create_genres_columns(movies)
-        #movies = self.drop_genres_column(movies)
+        movies = self.create_genres_columns(movies)
+        movies = self.drop_genres_column(movies)
         return movies
