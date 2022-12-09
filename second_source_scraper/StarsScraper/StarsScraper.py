cbbe621c8c815054b7627a249f64d3e1c7aa1685
Agustin Normand <normandagustin@gmail.com>
2022-12-09 01:40:00 -0300

    One more version of StarsScraper

--- second_source_scraper/StarsScraper/StarsScraper.py
+++ second_source_scraper/StarsScraper/StarsScraper.py
@@ -1,172 +1,187 @@
-from selenium.webdriver.support.wait import WebDriverWait
-
-import constants
-import time
-import requests
-from fake_headers import Headers
-import logging
-from bs4 import BeautifulSoup
+import queue
+from threading import Thread, Lock
+from fp.fp import FreeProxy
 from requests.adapters import HTTPAdapter, Retry
+import requests
+import time
+from fake_headers import Headers
+from bs4 import BeautifulSoup
+import pandas as pd
+import logging
+import constants
+
 
 class StarsScraper:
     def __init__(self):
-        self.scraped_actors = {}
-        self.last_request_timestamp = time.time()
-        self.logger = logging.getLogger("StarsScraper")
-        self.time_elapsed_waiting_http_response = 0
         self.scraped_movies = {}
+        self.actors_to_scrape_queue = queue.Queue()
+        self.scraped_actors = {}
+        self.logger = logging.getLogger("StarsScraper")
 
-        self.session = requests.Session()
+    def initialization(self, movies):
+        self.logger.debug("Movies to scrape stars {}".format(len(movies)))
+
+        self.actors_to_scrape = []
+        for movie in movies:
+            self.scraped_movies[movie["url_imdb"]] = movie["user_raiting"]
+            for (actor_name, actor_url) in movie["actors"]:
+                if (actor_name, actor_url) not in self.actors_to_scrape:
+                    if actor_name == "Lance Palmer": #Delete #TODO
+                        self.actors_to_scrape.append((actor_name, actor_url))
+
+        self.logger.debug("Actors to scrape {}".format(len(self.actors_to_scrape)))
+
+        for actor_to_scrape in self.actors_to_scrape:
+            self.actors_to_scrape_queue.put(actor_to_scrape)
+
+    def manage_scrape_actors(self):
+        lock = Lock()
+        threads = []
+        for worker_number in range(1): #Change to 100 #TODO
+            thread = Thread(target=self.scrape_actors, daemon=True, args=(lock,worker_number,))
+            threads.append(thread)
+            thread.start()
+
+        self.actors_to_scrape_queue.join()
+
+    def contains_actor_or_actress_filter(self, soup): #Esto va a ActorScraper
+        scroller = soup.find("div", {"class":"ipc-chip-list__scroller"})
+        if scroller == None:
+            return False
+
+        return ((scroller.find("button", {"id":"name-filmography-filter-actor"}) != None) or (scroller.find("button", {"id":"name-filmography-filter-actress"}) != None))
+
+    def contains_filmo_section_actor_or_actress(self, soup):#Esto va a ActorScraper
+        return ((soup.find("div", "filmo-section-actor") != None) or (soup.find("div", "filmo-section-actress") != None))
+
+    def contains_see_more_button(self, soup):#Esto va a ActorScraper
+        return soup.find("span", {"class":"ipc-see-more__text"}) != None
+
+    def get_actor_url(self, soup, actor_url):#Esto va a ActorScraper
+        div_filmo = soup.find("div", "filmo-section-actor")
+        if div_filmo != None:
+            return actor_url + "filmotype/actor"
+
+        div_filmo = soup.find("div", "filmo-section-actress")
+        if div_filmo != None:
+            return actor_url + "filmotype/actress"
+
+    def get_movies_from_acordeon(self, previous_movies):#Esto va a ActorScraper
+        movie_result_list = []
+        movies = previous_movies.find("ul", {"class": "ipc-metadata-list"}).find_all("li", {
+            "class": "ipc-metadata-list-summary-item"})
+        for movie in movies:
+            movie_dict = {}
+            movie_dict["movie_name"] = movie.find("a")["aria-label"]
+            movie_dict["movie_url"] = constants.IMDb_URL + movie.find("a")["href"].split("/?")[0]
+            movie_result_list.append(movie_dict)
+        return movie_result_list
+
+    def contains_filmo_section_self(self, soup):#Esto va a ActorScraper
+        return soup.find("div", {"class":"filmo-section-self"}) != None
+
+    def get_previous_movies(self, soup):
+        movies_section = soup.find("div", {"class": ["sc-4390696d-3", "khLxUB"]})
+        acordeon_title = movies_section.find("span", {"class": "ipc-accordion__item__title"}).find("li").get_text()
+        acordeon = None
+        if acordeon_title == "Upcoming":
+            acordeon = movies_section.find_all("div", {"class": "ipc-accordion__item__content"})[1]
+        if acordeon_title == "Previous":
+            acordeon = movies_section.find_all("div", {"class": "ipc-accordion__item__content"})[0]
+        #return
+
+
+    def parse_actor_page(self, text, actor_url, actor_name, lock):#Esto va a ActorScraper
+        try:
+            self.scraped_actors[actor_name] = {}
+            soup = BeautifulSoup(text, "html.parser")
+            if self.contains_filmo_section_actor_or_actress(soup):
+                previous_movies = self.get_previous_movies(soup)
+                self.scraped_actors[actor_name]["completed_scrape"] = not self.contains_see_more_button(previous_movies)
+                self.scraped_actors[actor_name]["url"] = self.get_actor_url(soup, actor_url)
+                self.scraped_actors[actor_name]["movies"] = self.get_movies_from_acordeon(previous_movies)
+                with lock:
+                    self.logger.debug(
+                        "Scraped {}, generic url {}, contains {}, completed_scrape {}, specific url {}, movies {}".format(
+                            actor_name, actor_url, "filmo_section_actor_or_actress",
+                            self.scraped_actors[actor_name]["completed_scrape"],
+                            self.scraped_actors[actor_name]["url"], self.scraped_actors[actor_name]["movies"]))
+            elif self.contains_actor_or_actress_filter(soup):
+                    with lock:
+                        self.logger.debug(
+                            "Scraping {}, generic url {}, not contain {}, but contains {}".format(
+                                actor_name, actor_url, "filmo_section_actor_or_actress",
+                                "actor_filter or actress_filter"
+                                ))
+            elif self.contains_filmo_section_self(soup):
+                    with lock:
+                        self.logger.debug(
+                            "Scraping {}, generic url {}, not contain {}, not contain {}, contains {}, is this an actor?".format(
+                                actor_name, actor_url, "filmo_section_actor_or_actress",
+                                "actor_filter or actress_filter",
+                                "filmo_section_self",
+                                ))
+                    self.scraped_actors[actor_name]["completed_scrape"] = True
+                    self.scraped_actors[actor_name]["url"] = actor_url
+                    self.scraped_actors[actor_name]["movies"] = []
+            else:
+                self.logger.debug("Scraping {}, generic url {}, not contemplated case".format(actor_name, actor_url))
+                self.scraped_actors[actor_name]["completed_scrape"] = True
+                self.scraped_actors[actor_name]["url"] = actor_url
+                self.scraped_actors[actor_name]["movies"] = []
+
+        except Exception as e:
+            self.logger.debug("Exception {} processing {}".format(e, actor_url))
+
+    def scrape_actors(self, lock, worker_number): #Esto va a ActorScraper
+        time.sleep(worker_number*2)
         retry = Retry(connect=5, backoff_factor=0.5)
         adapter = HTTPAdapter(max_retries=retry)
-        self.session.mount('http://', adapter)
-        self.session.mount('https://', adapter)
-        self.session.headers.update(Headers().generate())
+        session = requests.Session()
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+        session.headers.update(Headers().generate())
+        proxy = FreeProxy(country_id=['AR'], rand=True, timeout=0.5).get()
+        proxies = {
+            "http": proxy
+        }
+        last_request_timestamp = time.time()
 
-    def sleep_if_needed(self):
-        remaining_to_second_between_requests = constants.SECONDS_TO_SLEEP_BETWEEN_REQUESTS - (
-                    time.time() - self.last_request_timestamp)
-        if remaining_to_second_between_requests > 0:
-            time.sleep(remaining_to_second_between_requests)
+        while True:
+            actor_name, actor_url = self.actors_to_scrape_queue.get()
 
-    def request(self, url):
-        start_time_waiting_response = time.time()
+            remaining_to_second_between_requests = 0.1 - (
+                    time.time() - last_request_timestamp)
+            if remaining_to_second_between_requests > 0:
+                time.sleep(remaining_to_second_between_requests)
 
-        self.sleep_if_needed()
-        time_between_requests = time.time() - self.last_request_timestamp
-        r = self.session.get(url=url)
-        self.last_request_timestamp = time.time()
+            r = session.get(url=actor_url, proxies=proxies)
+            if r.status_code != 200:
+                self.logger.debug("Status code {}".format(r.status_code))
+            self.parse_actor_page(r.text, actor_url, actor_name, lock)
+            #self.logger.debug("Requests url {}, status {}, actor name {}".format(actor_url, r.status_code, actor_name))
 
-        time_elapsed = time.time() - start_time_waiting_response
-        self.time_elapsed_waiting_http_response += time_elapsed
-        self.logger.debug("New request to IMDb effectuated, "
-                          "URL {}, "
-                          "Status Code {}, "
-                          "Response len {}, "
-                          "Time elapsed waiting response {}, "
-                          "Time between requests {}".format(url, r.status_code, len(r.text), time_elapsed, time_between_requests))
-        return [r.status_code, r.text]
+            last_request_timestamp = time.time()
 
-    def get_actors(self, soup):
-        actors = []
-        divs_actors = soup.find_all("div", {"data-testid":"title-cast-item"})
-        for div_actor in divs_actors:
-            ancor = div_actor.find("a", {"data-testid":"title-cast-item__actor"})
-            actor_name = ancor.get_text()
-            href = ancor["href"].split("?")[0]
-            actor_url = constants.ACTORS_IMDb_URL + href + "filmotype/actor"
-            actors.append((actor_name, actor_url))
-        return actors
+            self.actors_to_scrape_queue.task_done()
 
-    def scrape_movie(self, movie_url):
-        status_code_movie, text_response_movie = self.request(movie_url)
+    def process_stars(self, movies):
+        #self.logger.debug("In process stars")
+        #self.logger.debug(movies)
+        start_time = time.time()
 
-        if status_code_movie != 200:
-            self.logger.error("Status code {}, Movie URL {}".format(status_code_movie, movie_url))
-            return None
+        self.initialization(movies)
 
-        soup = BeautifulSoup(text_response_movie, "html.parser")
+        self.manage_scrape_actors()
 
-        div = soup.find("div", {"data-testid": "hero-rating-bar__aggregate-rating__score"})
-        if div != None:
-            raiting = float(div.find("span").get_text())
-            self.scraped_movies[movie_url] = raiting
-        else:
-            self.scraped_movies[movie_url] = None
+        df = pd.DataFrame.from_records(self.scraped_actors)
+        df.to_csv("scraped_actors.csv", index=False)
 
-    def scrape_actor(self, actor_name, actor_url):
-        status_code_movie, text_response_movie = self.request(actor_url)
+        #self.logger.debug(self.scraped_actors)
+        total_time_elapsed = time.time() - start_time
 
-        if status_code_movie != 200:
-            self.logger.error("Status code {}, URL {}, Actor {}".format(status_code_movie, actor_url, actor_name))
-
-        try:
-            soup = BeautifulSoup(text_response_movie, "html.parser")
-            actor_name = soup.find("h5", {"itemprop":"name"}).find("a").get_text()
-            self.scraped_actors[actor_name] = []
-            movie_section = soup.find("section", {"id":"name-filmo-content"})
-            divs_movies = movie_section.find_all("div", {"class":"col-xs-12"})
-            if len(divs_movies) == 0:
-                if "actor" in actor_url:
-                    self.logger.debug("Movies in 0, scraping actress page")
-                    self.scrape_actor(actor_name, actor_url.replace("actor", "actress"))
-            else:
-                for div_movie in divs_movies:
-                    href = div_movie.find("a", {"class": "subpage"})["href"].split("/?")[0]
-                    url = constants.ACTORS_IMDb_URL + href
-                    self.scraped_actors[actor_name].append(url)
-        except Exception as e:
-            self.logger.error("Exception {}, scraping {}".format(e, actor_url))
+        self.logger.debug("Total time whole scraping {:.3g} minutes".format(total_time_elapsed / 60))
 
 
-    def scrape_actors_if_needed(self, actors):
-        for actor in actors:
-            actor_name = actor[0]
-            actor_url = actor[1]
-            if actor_name not in self.scraped_actors.keys():
-                self.logger.debug("Actor {} not in scraped_actors, scraping in what movies acted".format(actor_name))
-                self.scrape_actor(actor_name, actor_url)
-            else:
-                self.logger.debug("Actor {} was already scraped, not need to see in what movies acted".format(actor_name))
 
-    def get_movies_previous_to(self, movie_url, actor_movies):
-        movie_url_title = movie_url.split("imdb.com")[1]
-        found_movie = False
-        previous_movies = []
-        for actor_movie_url in actor_movies:
-            actor_movie_url_title = actor_movie_url.split("imdb.com")[1]
-            if found_movie:
-                previous_movies.append(actor_movie_url)
-            else:
-                if movie_url_title == actor_movie_url_title:
-                    found_movie = True
-                else:
-                    continue
-        if not found_movie:
-            self.logger.error("Movie never founded {} in {}".format(movie_url, actor_movies))
-        return previous_movies
-
-    def star_before(self, movie, actor_name):
-        self.logger.debug("Verifying if {}, was an actor before {}".format(actor_name, movie["movie_name"]))
-        previous_movies = self.get_movies_previous_to(movie["url_imdb"], self.scraped_actors[actor_name])
-
-        self.logger.debug("Previous movies of actor {} before {}".format(previous_movies, movie["movie_name"]))
-
-        for previous_movie in previous_movies:
-            if previous_movie not in self.scraped_movies:
-                self.logger.debug("Movie not in scraped_movies, scraping raiting of {}".format(previous_movie))
-                self.scrape_movie(previous_movie)
-            else:
-                self.logger.debug("Movie in scraped_movies, dont need to scrape raiting of {}".format(previous_movie))
-
-            if self.scraped_movies[previous_movie] == None:
-                self.logger.error("Movie without raiting {}".format(previous_movie))
-            else:
-                if self.scraped_movies[previous_movie] >= 7.5:
-                    self.logger.debug("Movie found that proves is a star, stop looking")
-                    return True
-        return False
-
-    def process_actors(self, actors, movie):
-        star_count = 0
-        for actor_tuple in actors:
-            actor_name = actor_tuple[0]
-            if self.star_before(movie, actor_name):
-                star_count += 1
-            #break
-
-        return star_count
-
-    def scrape_stars(self, soup, movie):
-        self.logger.debug("Processing {}".format(movie["movie_name"]))
-        actors = self.get_actors(soup)
-        self.logger.debug("Actors {}".format(actors))
-
-        self.scrape_actors_if_needed(actors)
-        #{'Robert Downey Jr.': ['https://m.imdb.com/title/tt18392014', 'https://m.imdb.com/title/tt2094116',
-        #                       'https://m.imdb.com/title/tt14404618',
-
-        star_count = self.process_actors(actors, movie)
-        movie["movie_star"] = star_count
-        self.logger.debug("Star Count of {} is {}".format(movie["movie_name"], star_count))
-        return movie
+        return movies
