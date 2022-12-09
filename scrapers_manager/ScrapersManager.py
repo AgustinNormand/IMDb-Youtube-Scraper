4b3f5730133e551914b755e94ee4197e88cd5d76
Agustin Normand <normandagustin@gmail.com>
2022-12-09 10:46:03 -0300

    Add checkpoint to avoid first and second source scrapers

--- scrapers_manager/ScrapersManager.py
+++ scrapers_manager/ScrapersManager.py
@@ -6,6 +6,9 @@ import threading
 import queue
 import time
 import logging
+import csv
+import ast
+
 
 class ScrapersManager():
 
@@ -51,3 +54,35 @@ class ScrapersManager():
         total_time_elapsed = time.time() - start_time
 
         self.logger.info("Total time whole scraping {:.3g} minutes".format(total_time_elapsed/60))
+
+    def begin_scrape_from_third_scraper(self):
+        start_time = time.time()
+
+        third_source_scraper_thread = threading.Thread(target=self.third_source_scraper.begin_scrape, daemon=True)
+        third_source_scraper_thread.start()
+
+        results_processor_thread = threading.Thread(target=self.results_processor.process_results, daemon=True)
+        results_processor_thread.start()
+
+        csv_filename = 'checkpoint.csv'
+        with open(csv_filename) as f:
+            reader = csv.DictReader(f)
+            lst = list(reader)
+        for item in lst:
+            item["actors"] = ast.literal_eval(item["actors"])
+            self.secondScraperQueue.put(item)
+        self.secondScraperQueue.put("NO_MORE_MOVIES")
+        print("File Readed")
+
+        third_source_scraper_thread.join()
+        results_processor_thread.join()
+
+        self.third_source_scraper.log_measurements()
+        self.results_processor.log_measurements()
+
+        total_time_elapsed = time.time() - start_time
+
+        self.logger.info("Total time whole scraping {:.3g} minutes".format(total_time_elapsed/60))
+
+        #ss = StarsScraper()
+        #ss.process_stars(lst)
